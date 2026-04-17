99b2d1eb16c018a480be7de0f2879a1c20233197
phaelpp3 <115674647+phaelpp3@users.noreply.github.com>
2026-04-17 16:38:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.post("/")
-def processar(data: dict):
-    texto = data.get("texto", "")
+class Input(BaseModel):
+    texto: str
+
+@app.get("/")
+def home():
+    return {"msg": "Python funcionando"}
+
+@app.post("/processar")
+def processar(data: Input):
     return {
-        "original": texto,
-        "maiusculo": texto.upper()
+        "original": data.texto,
+        "maiusculo": data.texto.upper()
     }
